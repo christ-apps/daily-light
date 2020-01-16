59144aae1e00b8f2404fef9bd1d34711b618ae22
cxy930123 <mail@xingyu1993.cn>
2020-01-16 15:50:33 +0800

    排行榜界面

---
+++ src/components/ranking-item.vue
@@ -0,0 +1,66 @@
+<template>
+  <view class="ranking-item">
+    <view class="ranking">{{ranking}}</view>
+    <image class="avatar" :src="avatar" />
+    <view class="nickname">{{nickname}}</view>
+    <block v-if="star || star === 0">
+      <uni-icons type="star-filled" :size="30" color="#d51e7b" />
+      <view class="star">x{{star}}</view>
+    </block>
+    <view class="score">{{score}}</view>
+  </view>
+</template>
+
+<script>
+import UniIcons from "@/components/uni-icons/uni-icons";
+export default {
+  components: {
+    UniIcons
+  },
+
+  props: {
+    ranking: Number,
+    avatar: String,
+    nickname: String,
+    star: Number,
+    score: Number
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.ranking-item {
+  padding: 15rpx;
+  display: flex;
+  align-items: center;
+}
+
+.ranking {
+  min-width: 3ch;
+  text-align: center;
+}
+
+.avatar {
+  margin-left: 15rpx;
+  width: 100rpx;
+  height: 100rpx;
+  border-radius: 50%;
+}
+
+.nickname {
+  margin: 0 15rpx;
+  flex: 1;
+}
+
+.star {
+  min-width: 4ch;
+}
+
+.score {
+  margin-left: 15rpx;
+  min-width: 5ch;
+  text-align: right;
+  font-size: 1.5rem;
+  color: $uni-color-primary;
+}
+</style>
--- src/pages/ranking.vue
+++ src/pages/ranking.vue
@@ -1,9 +1,153 @@
 <template>
-  
+  <view class="page">
+    <view class="header">
+      <image class="cover" :src="cover" />
+      <view class="tabs">
+        <view class="tab" :class="{ 'tab__selected': type === 'total' }" @tap="type = 'total'">总排行</view>
+        <view class="tab" :class="{ 'tab__selected': type === 'week' }" @tap="type = 'week'">周排行</view>
+      </view>
+    </view>
+    <ranking-item
+      :ranking="userRanking"
+      :avatar="user.avatar"
+      :nickname="user.nickname"
+      :star="type === 'total' ? user.count : null"
+      :score="user[`${type}Count`]"
+    />
+    <view class="ranking-list" v-if="type === 'total'">
+      <view
+        class="ranking-item--container"
+        v-for="(user, index) in totalRanking"
+        :key="user.userid"
+      >
+        <ranking-item
+          :ranking="index + 1"
+          :avatar="user.avatar"
+          :nickname="user.nickname"
+          :star="user.count"
+          :score="user.totalCount"
+        />
+      </view>
+    </view>
+    <view class="ranking-list" v-if="type === 'week'">
+      <view class="ranking-item--container" v-for="(user, index) in weekRanking" :key="user.userid">
+        <ranking-item
+          :ranking="index + 1"
+          :avatar="user.avatar"
+          :nickname="user.nickname"
+          :score="user.weekCount"
+        />
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
+import RankingItem from "@/components/ranking-item";
+import Mock from "mockjs";
 export default {
-  
-}
+  components: {
+    RankingItem
+  },
+
+  data() {
+    return {
+      // 排行榜类型：week/total
+      type: "total",
+      // 封面图片地址
+      cover: Mock.Random.image("700x375"),
+      // 用户排行记录
+      ranking: Array.from({
+        length: Mock.mock("@natural(1, 200)")
+      }).map((_, index) =>
+        Mock.mock({
+          userid: index,
+          avatar: "@image()",
+          nickname: "@cname()",
+          count: "@natural(0, 5)",
+          weekCount: "@natural(0, 20)",
+          totalCount: "@natural(20, 9999)"
+        })
+      )
+    };
+  },
+
+  computed: {
+    totalRanking() {
+      if (!Array.isArray(this.ranking)) return null;
+      return this.ranking
+        .slice()
+        .sort((item1, item2) => item2.totalCount - item1.totalCount);
+    },
+
+    weekRanking() {
+      if (!Array.isArray(this.ranking)) return null;
+      return this.ranking
+        .slice()
+        .sort((item1, item2) => item2.weekCount - item1.weekCount);
+    },
+
+    user() {
+      if (!Array.isArray(this.ranking)) return null;
+      const userid = this.ranking[0].userid;
+      return this.ranking.find(user => user.userid === userid);
+    },
+
+    userRanking() {
+      if (!this.user) return null;
+      return (
+        this[`${this.type}Ranking`].findIndex(
+          user => user.userid === this.user.userid
+        ) + 1
+      );
+    }
+  }
+};
 </script>
+
+<style lang="scss" scoped>
+.header {
+  position: relative;
+}
+
+.cover {
+  display: block;
+  width: 750rpx;
+  height: 375rpx;
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), #fff);
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: -1px;
+}
+
+.tab {
+  width: 200rpx;
+  min-height: 80rpx;
+  line-height: 80rpx;
+  border: 1px solid $uni-color-primary;
+  border-bottom: none;
+  border-radius: 15rpx 15rpx 0 0;
+  text-align: center;
+  color: $uni-color-primary;
+  background-color: $uni-bg-color;
+}
+
+.tab__selected {
+  color: $uni-bg-color;
+  background-color: $uni-color-primary;
+}
+
+.ranking-list {
+  border-top: 20rpx solid $uni-bg-color-grey;
+}
+
+.ranking-item--container {
+  border-bottom: 1px solid $uni-border-color;
+}
+</style>
